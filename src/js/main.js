dc65179f25a8e8dc11add888945f90ec6c7c3d06
s-lll-k <sylik3210@gmail.com>
2022-06-19 22:54:43 +0600

    main page, header white version to main page

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-// $(function () {
-    
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    const slider = new Swiper('.swiper', {
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false,
+        },
+        effect: "fade",
+        loop: true,
+        allowTouchMove: false
+    });
+})
